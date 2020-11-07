c5254c75d8e7ee0cc6aa5ca41e8f613b7fb21693
Juanma <jmlpdr.93@gmail.com>
2020-11-07 16:40:42 +0000

    inicializando REACT básico

---
+++ README.md
@@ -0,0 +1,13 @@
+# MERN - Simple Setup Check
+
+#### What you need to run this code
+1. Node (12.18.3)
+2. NPM (6.14.6) or Yarn (1.22.5)
+3. MongoDB (4.4.1)
+
+####  How to run this code
+1. Clone this repository
+2. Open command line in the cloned folder, 
+   - To install dependencies, run ```  npm install  ``` or ``` yarn ```
+   - To run the application for development, run ```  npm run development  ``` or ``` yarn development ```
+4. Open [localhost:3000](http://localhost:3000/) in the browser
---
+++ server/server.js
@@ -0,0 +1,29 @@
+import path from 'path';
+import express from 'express';
+import { MongoClient } from 'mongodb';
+import template from './../template';
+import devBundle from './devBundle';
+
+const app = express();
+devBundle.compile(app);
+
+const CURRENT_WORKING_DIR = process.cwd();
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
+
+app.get('/', (req, res) => {
+    res.status(200).send(template());
+});
+
+let port = process.env.PORT || 3000;
+app.listen(port, function onStart(err) {
+    if (err) {
+        console.log(err);
+    }
+    console.info('Server started on port %s.', port);
+});
+
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mernSimpleSetup';
+MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true },(err, db)=>{
+  console.log("Connected successfully to mongodb server");
+  db.close();
+});
